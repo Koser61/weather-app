526c0286ea2ab00fa4c0c588b147c4202fe90b50
Koser61 <kosinskipiotr.98@gmail.com>
2022-03-30 17:17:47 +0200

    Add current weather data fetching

--- src/App.js
+++ src/App.js
@@ -9,6 +9,37 @@ import './App.scss';
 
 const App = () => {
   const [searchString, changeSearchString] = useState('');
+  const [currentWeatherData, changeCurrentWeatherData] = useState({});
+
+  const fetchCurrentWeatherData = async () => {
+    if(searchString !== '') {
+      const options = {
+        method: 'GET',
+        headers: {
+          'X-RapidAPI-Host': process.env.REACT_APP_API_HOST,
+          'X-RapidAPI-Key': process.env.REACT_APP_API_KEY
+        }
+      };
+      const api = {
+        url: 'https://community-open-weather-map.p.rapidapi.com',
+        endpoint: '/weather',
+        urlParams: `q=${searchString}&units=metric`,
+      };
+      const url = `${api.url}${api.endpoint}?${api.urlParams}`;
+  
+      try {
+        const response = await fetch(url, options);
+  
+        if(response.ok) {
+          const data = await response.json();
+
+          changeCurrentWeatherData(data);
+        }
+      } catch(error) {
+        console.log(error);
+      }
+    }
+  };
 
   return (
     <>
@@ -18,7 +49,7 @@ const App = () => {
             value={searchString}
             onChangeFunc={changeSearchString}
           />
-          <SearchButton />
+          <SearchButton onClickFunc={fetchCurrentWeatherData} />
         </SearchBar>
       </Header>
     </>
--- src/components/SearchButton/SearchButton.js
+++ src/components/SearchButton/SearchButton.js
@@ -1,20 +1,21 @@
-//import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlassLocation } from '@fortawesome/free-solid-svg-icons';
 
 import styles from './SearchButton.module.scss';
 
-const SearchButton = () => (
+const SearchButton = ({ onClickFunc }) => (
   <button
     className={styles.component}
+    onClick={() => onClickFunc()}
   >
     <FontAwesomeIcon icon={faMagnifyingGlassLocation} />
   </button>
 );
 
 SearchButton.propTypes = {
-  
+  onClickFunc: PropTypes.func.isRequired,
 };
 
 export default SearchButton;
