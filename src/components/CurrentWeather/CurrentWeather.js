89c373f316f10b8b10c6e314443077909b06acb3
Koser61 <kosinskipiotr.98@gmail.com>
2022-04-02 13:44:58 +0200

    Add CurrentWeather prototype

--- src/components/CurrentWeather/CurrentWeather.js
+++ src/components/CurrentWeather/CurrentWeather.js
@@ -1,13 +1,49 @@
 import PropTypes from 'prop-types';
 
 import Card from '../Card/Card';
+import WeatherParam from '../WeatherParam/WeatherParam';
+import WeatherIcon from '../WeatherIcon/WeatherIcon';
+import WindDirection from '../WindDirection/WindDirection';
 
-//import styles from './CurrentWeather.module.scss';
+import styles from './CurrentWeather.module.scss';
 
 const CurrentWeather = ({ data }) => {
   return (
     <Card>
-      <p>CurrentWeather</p>
+      <div className={styles.header}>
+        <h1>London, GB</h1>
+        <p>Wednesday 30 March</p>
+      </div>
+      <div className={styles.grid}>
+        <div className={styles.mainParams}>
+          <WeatherParam description='thunderstorm with heavy drizzle'>
+            <WeatherIcon iconCode='11d' />
+          </WeatherParam>
+          <WeatherParam description='Feels like 44°C'>
+            45°C
+          </WeatherParam>
+        </div>
+        <div className={styles.otherParams}>
+          <WeatherParam small description='Visibility'>
+            999m
+          </WeatherParam>
+          <WeatherParam small description='Cloudiness'>
+            75%
+          </WeatherParam>
+          <WeatherParam small description='Pressure'>
+            1004hPa
+          </WeatherParam>
+          <WeatherParam small description='Humidity'>
+            78%
+          </WeatherParam>
+          <WeatherParam small description='Wind direction'>
+            <WindDirection degree={45} />
+          </WeatherParam>
+          <WeatherParam small description='Wind speed'>
+            100m/s
+          </WeatherParam>
+        </div>
+      </div>
     </Card>
   );
 };
