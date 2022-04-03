b0cb05f2d7a2bfac53dcb6d6891b304e3b7b9df6
Koser61 <kosinskipiotr.98@gmail.com>
2022-04-03 12:07:03 +0200

    Fix WindDirection (it showed opposite direction)

--- src/components/WindDirection/WindDirection.js
+++ src/components/WindDirection/WindDirection.js
@@ -1,11 +1,11 @@
 import PropTypes from 'prop-types';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUpLong } from '@fortawesome/free-solid-svg-icons';
+import { faDownLong } from '@fortawesome/free-solid-svg-icons';
 
 const WindDirection = ({ degree }) => (
   <>
-    <FontAwesomeIcon icon={faUpLong} transform={{ rotate: degree }} />
+    <FontAwesomeIcon icon={faDownLong} transform={{ rotate: degree }} />
   </>
 );
 
