4e77ac369517044ae256507e26490534e4aabb71
Koser61 <kosinskipiotr.98@gmail.com>
2022-04-10 18:18:32 +0200

    Fix serving react app from proxy.js [2]

--- proxy.js
+++ proxy.js
@@ -8,7 +8,7 @@ const app = express()
 
 app.use(cors())
 
-app.use(express.static('public'));
+app.use(express.static('build'));
 
 app.get('/current', (req, res) => {
   const searchString = req.query.q
