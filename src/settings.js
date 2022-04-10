a5b0cc917d4459820f282af6e751a5d06a81fd99
Koser61 <kosinskipiotr.98@gmail.com>
2022-04-10 17:58:04 +0200

    Change api proxy url in settings.js

--- src/settings.js
+++ src/settings.js
@@ -1,5 +1,5 @@
 export const api = {
-  url: '//' + window.location.hostname + (window.location.hostname === 'localhost' ? ':8000' : `${process.env.PORT}`),
+  url: '//' + window.location.hostname + ':8000',
   endpoints: {
     current: 'current',
     forecast: 'forecast'
