3e71763de0710760b323ba364f60eaccc01dac48
Koser61 <kosinskipiotr.98@gmail.com>
2022-04-25 12:31:04 +0200

    Modify SearchButton onClick event

--- src/components/SearchButton/SearchButton.js
+++ src/components/SearchButton/SearchButton.js
@@ -5,17 +5,24 @@ import { faMagnifyingGlassLocation, faSpinner } from '@fortawesome/free-solid-sv
 
 import styles from './SearchButton.module.scss';
 
-const SearchButton = ({ onClickFunc, loading, error }) => (
-  <button
-    className={error ? styles.error : styles.component}
-    onClick={() => onClickFunc()}
-    role='search'
-    aria-label='search button'
-  >
-    {loading ? <FontAwesomeIcon icon={faSpinner} spin />
-             : <FontAwesomeIcon icon={faMagnifyingGlassLocation} />}
-  </button>
-);
+const SearchButton = ({ onClickFunc, loading, error }) => {
+  const handleClick = (event) => {
+    event.preventDefault();
+    onClickFunc();
+  };
+
+  return (
+    <button
+      className={error ? styles.error : styles.component}
+      onClick={(event) => handleClick(event)}
+      role='search'
+      aria-label='search button'
+    >
+      {loading ? <FontAwesomeIcon icon={faSpinner} spin />
+              : <FontAwesomeIcon icon={faMagnifyingGlassLocation} />}
+    </button>
+  );
+};
 
 SearchButton.propTypes = {
   onClickFunc: PropTypes.func.isRequired,
